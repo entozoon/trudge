019523e9c09228e71ec732400d91c6a99da2e1d2
MichaelCook <michael@evoluted.net>
2017-05-16 13:14:46 +0100

    Phaser-ce (2.7.7) and simple js file concatenation

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,46 @@
-'use strict';
+"use strict";
 
 //
 // Compiles into /dist as vanilla Javascript (es2015)
 //
 
-console.log('hello!');
+var game = void 0,
+    graphics = void 0;
 
-// ES6 y'all!
-[1, 2, 3, 4].map(function (number) {
-	console.log(number);
-});
+window.onload = function () {
+  // (after images)
+  game = new Phaser.Game({
+    width: 200, // temporary
+    height: 200,
+    antialias: false,
+    renderer: Phaser.CANVAS, // force canvas. Phaser.AUTO is an alternative
+    state: {
+      preload: preload,
+      create: create,
+      update: update,
+      render: render
+    }
+  });
+};
+
+var preload = function preload() {
+  // Images..
+};
+var create = function create() {
+  game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+  game.scale.parentIsWindow = true;
+  graphics = game.add.graphics(0, 0);
+};
+var update = function update() {};
+var render = function render() {
+  // Clear
+  graphics.clear();
+
+  // Bg
+  graphics.beginFill(0xFFAA00, 1);
+  graphics.drawRect(0, 0, game.width, game.height);
+  graphics.endFill();
+};
+'use strict';
+
+console.log('this is a thing');
--- src/app.js
+++ src/app.js
@@ -2,9 +2,47 @@
 // Compiles into /dist as vanilla Javascript (es2015)
 //
 
-console.log('hello!');
+let game,
+  graphics;
 
-// ES6 y'all!
-[1, 2, 3, 4].map(number => {
-	console.log(number);
-});
+window.onload = () => { // (after images)
+  game = new Phaser.Game({
+    width:     200, // temporary
+    height:    200,
+    antialias: false,
+    renderer:  Phaser.CANVAS, // force canvas. Phaser.AUTO is an alternative
+    state: {
+      preload: preload,
+      create:  create,
+      update:  update,
+      render:  render
+    }
+  });
+}
+
+
+const preload = () => {
+  // Images..
+}
+const create = () => {
+  game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+	game.scale.parentIsWindow = true;
+	graphics = game.add.graphics(0, 0);
+}
+const update = () => {
+}
+const render = () => {
+	// Clear
+	graphics.clear();
+
+	// Bg
+	graphics.beginFill(0xFFAA00, 1);
+	graphics.drawRect(
+		0,
+		0,
+		game.width,
+		game.height
+	);
+	graphics.endFill();
+
+}
