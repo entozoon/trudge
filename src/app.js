258f94e39095dd2ade206785e6e055fca664efe7
MichaelCook <michael@evoluted.net>
2017-05-18 12:59:42 +0100

    Tinkering with collisions but it's getting circular..

--- src/app.js
+++ src/app.js
@@ -1,36 +1,34 @@
 //
 // Concatenates all /src files into /build/app.js as compiles to vanilla Javascript (es2015)
 //
-let game,
-  graphics,
-  creatures = [],
-  physicalGroup;
+let game, graphics, creatures = [], physicalGroup;
 
-window.onload = () => { // (after images)
+window.onload = () => {
+  // (after images)
   game = new Phaser.Game({
-    width:     200, // temporary
-    height:    200,
+    width: 200, // temporary
+    height: 200,
     antialias: false,
-    renderer:  Phaser.CANVAS, // force canvas. Phaser.AUTO is an alternative
+    renderer: Phaser.CANVAS, // force canvas. Phaser.AUTO is an alternative
     state: {
       preload: preload,
-      create:  create,
-      update:  update,
-      render:  render
+      create: create,
+      update: update,
+      render: render
     }
   });
-}
+};
 
 const preload = () => {
   // Images..
   game.load.image('heroSprite', 'sprites/hero.png');
   game.load.image('creatureSprite', 'sprites/creature.png');
-}
+};
 
 const create = () => {
   game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-	game.scale.parentIsWindow = true;
-	graphics = game.add.graphics(0, 0);
+  game.scale.parentIsWindow = true;
+  graphics = game.add.graphics(0, 0);
 
   // NB: Only sprites can be made part of the physics system, apparently!
   physicalGroup = game.add.group();
@@ -38,62 +36,66 @@ const create = () => {
   physicalGroup.physicsBodyType = Phaser.Physics.ARCADE;
 
   // Creatures
-  creatures.push(new Creature({
-    x: 100,
-    y: 100,
-    velocity: { x: 100, y: 10 }
-  }));
+  creatures.push(
+    new Creature({
+      x: 100,
+      y: 100,
+      velocity: { x: 100, y: 10 }
+    })
+  );
 
-  creatures.push(new Creature({
-    x: 200,
-    y: 100,
-    velocity: { x: 0, y: 0 }
-  }));
+  creatures.push(
+    new Creature({
+      x: 200,
+      y: 100,
+      velocity: { x: 0, y: 0 }
+    })
+  );
 
-  creatures.push(new Creature({
-    x: 300,
-    y: 100,
-    velocity: { x: 0, y: 0 }
-  }));
+  creatures.push(
+    new Creature({
+      x: 300,
+      y: 100,
+      velocity: { x: 0, y: 0 }
+    })
+  );
 
-  creatures.push(new Creature({
-    x: 400,
-    y: 100,
-    velocity: { x: -100, y: 10 }
-  }));
-/*
-  creatures.push(new Creature({
-    sprite: game.add.sprite(41, 42, 'heroSprite'),
-    x: 700,
-    y: 100,
-    velocity: { x: -50, y: 0 }
-  }));
-*/
-}
+  creatures.push(
+    new Creature({
+      x: 400,
+      y: 100,
+      velocity: { x: -100, y: 10 }
+    })
+  );
+
+  creatures.push(
+    new Creature({
+      sprite: game.add.sprite(41, 42, 'heroSprite'),
+      x: 500,
+      y: 100,
+      velocity: { x: -50, y: 0 }
+    })
+  );
+};
 
 const update = () => {
   // Creatures
   creatures.forEach(thing => {
     thing.update();
   });
-}
+};
 
 const render = () => {
-	// Clear
-	graphics.clear();
+  // Clear
+  graphics.clear();
 
-	// Bg
-	graphics.beginFill(0xEEEEEE, 1);
-	graphics.drawRect(
-		0,
-		0,
-		game.width,
-		game.height
-	);
-	graphics.endFill();
+  // Bg
+  graphics.beginFill(0xeeeeee, 1);
+  graphics.drawRect(0, 0, game.width, game.height);
+  graphics.endFill();
 
   // Creatures
   creatures.forEach(thing => {
     thing.render();
   });
-}
+};
--- src/objects/creature.js
+++ src/objects/creature.js
@@ -1,14 +1,14 @@
 class Creature {
   constructor(options) {
     // Create empty options just in case
-    options = (options ? options : {});
+    options = options ? options : {};
 
     // Sprite
-    this.sprite = (options.sprite ? options.sprite : game.add.sprite(41, 42, 'creatureSprite'));
+    this.sprite = options.sprite ? options.sprite : game.add.sprite(41, 42, 'creatureSprite');
 
     // Physics
     game.physics.arcade.enableBody(this.sprite);
-  	//this.sprite.body.collideWorldBounds = true; // I'm _very_ not convinced the AoE is right on load
+    //this.sprite.body.collideWorldBounds = true; // I'm _very_ not convinced the AoE is right on load
     physicalGroup.add(this.sprite);
 
     // Bounding box (width, height, x, y offset)
@@ -18,9 +18,12 @@ class Creature {
     //this.sprite.body.setCircle(20, 0, 0); // r, x, y
 
     // Position
-    this.sprite.x = (options.x ? options.x : 99);
-    this.sprite.y = (options.y ? options.y : 99);
-    this.sprite.body.velocity = (options.velocity ? options.velocity : null);
+    this.sprite.x = options.x ? options.x : 99;
+    this.sprite.y = options.y ? options.y : 99;
+    this.sprite.body.velocity = options.velocity ? options.velocity : null;
+
+    // Pass 'this' context to sprite for use with wasTouched
+    this.sprite.mother = this;
   }
 
   getSprite() {
@@ -32,18 +35,38 @@ class Creature {
     return true;
   }
 
-  collideCallback(a, b) {
-    a.tint = 0xff0000;
-    b.tint = 0x0000ff;
+  wasTouched() {
+    this.sprite.tint = 0x440000;
+  }
 
+  collideCallback(a, b) {
+    // a (this) and b aren't reliable in that it can't distinguish which hit which.
+    // So just be dead ambivolent about it
+    //a.tint = 0xff0000;
+    //b.tint = 0x000000;
     // if (!a.body.touching.none) {
     //   console.log(a.body.touching);
     // }
+    //
+
+    if (a.mother) {
+      //console.log(a.mother);
+      a.mother.wasTouched();
+    }
+    if (b.mother) {
+      b.mother.wasTouched();
+    }
   }
 
   update() {
     //game.physics.arcade.collide(player, platforms, hitPlatform(), isPlatformSolid(), this);
-    game.physics.arcade.collide(this.sprite, physicalGroup, this.collideCallback, this.processCallback, this);
+    game.physics.arcade.collide(
+      this.sprite,
+      physicalGroup,
+      this.collideCallback,
+      this.processCallback,
+      this
+    );
     // .overlap is also a thing. Detects overlap without causing a collision as such
   }
 
